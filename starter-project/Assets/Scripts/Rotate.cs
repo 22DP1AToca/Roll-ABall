618fb1604d86eb81de31c81b1750a960042e84c8
NIKSUH1 <turhaa111@outlook.com>
2025-02-13 10:11:39 +0200

    initial project

---
+++ starter-project/Assets/Scripts/Rotate.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15,35,45) * Time.deltaTime);
+
+
+    }
+}
